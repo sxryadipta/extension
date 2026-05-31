b999f7ce15a2156d939b601b2b3ee5a5f20cb636
Suryadipta Ghosh <gsuryadipta@gmail.com>
2026-05-31 13:26:00 +0530

    Phase 3: Creating a problem.md file automatically

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-//this part sends the title to popup.js file.
+//this part fetches and sends the problem title to popup.js file.
 
 console.log("Content script loaded!", document.querySelector('[data-cy="question-title"]'));
 
@@ -7,4 +7,15 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     const el = document.querySelector('.text-title-large a');  // target the <a> tag
     sendResponse({ title: el ? el.innerText.trim() : null });
   }
+
+  if (message.type === "GET_DESCRIPTION") {
+    const el = document.querySelector('[data-track-load="description_content"]');
+    sendResponse({ description: el ? el.innerText : null });
+  }
 });
+
+
+// this part fetches the problem description and converts into a markdown.
+
+const descDiv = document.querySelector('[data-track-load="description_content"]');
+const markdown = descDiv.innerText;
